f8aadda4b6cb362fbcd93fe2ad2765d601f12b00
Antti Nilakari <antti.nilakari@gmail.com>
2014-11-24 11:08:47 +0200

    Added main python script and word list

---
+++ kapunaattori.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+from random import choice
+
+WORDLIST = "kotus_sanat.txt"
+
+
+def create_cache_file(input, output_filename, filter):
+	outfile = open(output_filename, "w")
+	for line in input:
+		stripped = line.rstrip()
+		if filter(stripped):
+			outfile.write(stripped)
+			outfile.write("\n")
+
+def create_cache_dir(cache_location):
+	try:
+		os.mkdir(cache_location)
+		print("Cache does not exist, creating", file=sys.stderr)
+	except OSError:
+		return
+	
+	# Cache files
+	with open(WORDLIST, "r") as word_file:
+		words = word_file.readlines()
+		filters = [
+			# Words that end in "ka"
+			["{}/ka.txt".format(cache_location),   lambda s: s.endswith("ka")],
+
+			# Words that start with "pu"
+			["{}/pu.txt".format(cache_location),   lambda s: s.startswith("pu")],
+
+			# Just for fun, but we're not getting random results from here
+			["{}/kapu.txt".format(cache_location), lambda s: "kapu" in s],
+		]
+
+		for f in filters:
+			print("Caching {}".format(f[0]), file=sys.stderr)
+			create_cache_file(words, *f)
+
+
+# File cache dir
+file_location = os.path.dirname(os.path.realpath(__file__))
+cache_location = "{}/cache".format(file_location)
+create_cache_dir(cache_location)
+
+ka = open("{}/ka.txt".format(cache_location), "r").readlines()
+pu = open("{}/pu.txt".format(cache_location), "r").readlines()
+
+print("{}{}".format(choice(ka).rstrip(), choice(pu).rstrip()))
