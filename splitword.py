f3b54895e801c3947e09043cd415474ad72865d7
Antti Nilakari <antti.nilakari@gmail.com>
2014-11-25 21:20:20 +0200

    Removed superfluous random.shuffle

--- splitword.py
+++ splitword.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from random import shuffle
 
 # Splits a word into multiple parts
 def split_word(word):
 	if len(word) < 2:
 		raise Exception("You obviously need at least two letters to split a word")
 	split_indexes = list(range(1, len(word)))
-	shuffle(split_indexes)
 	for i in split_indexes:
 		first_part = word[:i]
 		second_part = word[i:]
