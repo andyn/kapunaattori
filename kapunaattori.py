47f136dcb8b354e752ad8f77fd4adc99377b0283
Antti Nilakari <antti.nilakari@gmail.com>
2014-11-25 20:03:37 +0200

    Now allows arbitrary words and tries to split in multiple positions

--- kapunaattori.py
+++ kapunaattori.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
+# encoding: utf-8
+
 
 from __future__ import print_function
 import os
 from random import choice
+from splitword import split_word
 import sys
-
 from wordcache import WordCache
 
-# The directory that contains this script file
+
+# "This directory" is the directory that contains this script file
 THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+# Cache and word list directories. Change if you want them somewhere else
 CACHE_DIRECTORY = "{}/cache".format(THIS_DIRECTORY)
 WORDLIST_DIRECTORY = "{}/wordlists".format(THIS_DIRECTORY)
 
-params = sys.argv
 
 cache = WordCache(CACHE_DIRECTORY, WORDLIST_DIRECTORY)
-prefixes = cache.open_prefix("ka").readlines()
-suffixes = cache.open_suffix("pu").readlines()
 
-print("{}{}".format(
-	choice(prefixes).rstrip(),
-	choice(suffixes).rstrip()
-))
+# Use either a user-supplied word or the default, "kapu"
+if len(sys.argv) > 1:
+	word = sys.argv[1]
+else:
+	word = "kapu"
+
+# Try ones split in the middle first
+for (first, second) in split_word(word):
+	try:
+		prefixes = cache.open_prefix(first).readlines()
+		suffixes = cache.open_suffix(second).readlines()
+
+		print("{}{}".format(
+			choice(prefixes).rstrip(),
+			choice(suffixes).rstrip()))
+		break
+
+	except IndexError:
+		# Either of the two random.choices got an empty string,
+		# so try again with another permutation
+		pass
+
