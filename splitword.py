327428de0267de773a850daa9d376891fe02308f
Antti Nilakari <antti.nilakari@iki.fi>
2018-10-31 21:33:15 +0200

    Allow splitting to zero-length parts

--- splitword.py
+++ splitword.py
@@ -4,10 +4,7 @@
 
 # Splits a word into multiple parts
 def split_word(word):
-    if len(word) < 2:
-        raise Exception(
-            "You obviously need at least two letters to split a word")
-    split_indexes = list(range(1, len(word)))
+    split_indexes = list(range(0, len(word)))
     for i in split_indexes:
         first_part = word[:i]
         second_part = word[i:]
